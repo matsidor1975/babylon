3815d3a398569816f18277996789c67660be44a0
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2025-03-28 12:44:32 -0300

    chore: update MaxFinalizedRewardedBlocksPerEndBlock to 500

--- x/finality/types/constants.go
+++ x/finality/types/constants.go
@@ -1,8 +1,8 @@
 package types
 
 const (
-	// Setting max amount of finalized and rewarded blocks per EndBlock to 10000,
+	// Setting max amount of finalized and rewarded blocks per EndBlock to 500,
 	// with this setting, block processing times are <1s, even if BTC finality
 	// stalls a lot
-	MaxFinalizedRewardedBlocksPerEndBlock = uint64(10000)
+	MaxFinalizedRewardedBlocksPerEndBlock = uint64(500)
 )
